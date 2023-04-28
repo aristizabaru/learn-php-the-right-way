4c5c6a698457a6161d9517eca25b48dd3349b854
Andrés Aristizábal <andres.aristizabal@gmail.com.com>
2023-04-28 11:21:48 +0000

    feat: add lesson 10

---
+++ 10_expressions/1_expressions.php
@@ -0,0 +1,21 @@
+<?php // Expresiones en PHP
+
+/**
+ * Expresiones en PHP
+ * La expresiones son los bloques de construcción más importantes de PHP.
+ * En PHP casi todo lo que se escribe es una expresión. La manera más
+ * simple y acertada de definir lo que es una expresión es
+ * «cualquier cosa que tiene un valor o evalua a un valor».
+ * 
+ * PHP es un lenguaje orientado a expresiones
+ * 
+ * Documentación: https://www.php.net/manual/es/language.expressions.php
+ */
+
+// Expresiones
+
+$x = 5; // expresión que evalua a 5.
+
+$x = $y;
+
+$z = $x === $y; // expresión que evalua a true(1)
